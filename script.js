4b36725c7fdbda7a090664285551e219c338dd5c
themadscientist02 <71499011+themadscientist02@users.noreply.github.com>
2022-11-26 19:05:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute(){
+
+    var p= document.getElementById("principal").value;
+
+    if( p<=0){
+        alert("Enter a positive number")
+    }
+    if(p == null){
+        alert("Enter a positive number")
+    }
+
+    var r= document.getElementById("rate").value;
+    var t= document.getElementById("noy").value;
     
-}
-        
+    y= new Date().getFullYear() + parseInt(t);
+
+    var SI= (p*r*t)/100;
+    if(p>0){
+
+    //document.getElementById("result").value = "If you deposite "+p+", at an interest rate of "+r+" You will recieve "+SI;
+    document.getElementById("ans1").innerHTML = "If you deposite ";
+    document.getElementById("s1").innerHTML = p+"," ;
+    document.getElementById("ans2").innerHTML = "At an interest rate of ";
+    document.getElementById("s2").innerHTML = r+"%" ;
+    document.getElementById("ans3").innerHTML = "You will recieve ";
+    document.getElementById("s3").innerHTML = SI+"." ;
+    document.getElementById("ans4").innerHTML = "In the year ";
+    document.getElementById("s4").innerHTML = y ;
+
+    }
+
+    }  
